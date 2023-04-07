d9492d97cc67afa314c35e83008e734bd5072afc
ShinYeaChan <88381563+YeaChan05@users.noreply.github.com>
2023-04-07 15:54:15 +0900

    Revert "[FIX] examinate unnecessary parameter"
    This reverts commit 7287388de5123b93d198ab84b1cbf585b7079a36.

--- src/DeleteTodo.js
+++ src/DeleteTodo.js
@@ -8,7 +8,7 @@ class DeleteTodo extends React.Component {
     }
 
     deleteForCompleted = () => {
-        this.deleteForCompleted();
+        this.deleteForCompleted(this.state.item);
     }
 
     render() {
